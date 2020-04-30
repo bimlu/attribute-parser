5d94fffdbe125e83cb0fbae12b2b57840478b125
robinsrz <35960806+robinsrz@users.noreply.github.com>
2020-04-30 11:59:41 +0530

    Update main.cpp
    remove ParsedTag struct

--- main.cpp
+++ main.cpp
@@ -9,13 +9,8 @@ using query_type = std::string;
 
 struct ParsedQuery
 {
-    query_type tag{};
-    query_type attribute{};
-};
-
-struct ParsedTags
-{
     container_type tagArray;
+    query_type attribute{};
 };
 
 container_type getHRMLSourceCode(int numberOfLines);
@@ -23,9 +18,7 @@ void printHRMLSourceCode(container_type HRMLSourceCode);
 container_type getQueries(int numberOfQuery);
 void printQueries(container_type queries);
 ParsedQuery getParsedQuery(query_type query);
-ParsedTags getParsedTags(std::string tag);
-void printParsedTags(ParsedTags parsedTags);
-std::string getLineWithQueryTag(std::string tag, container_type HRMLSourceCode);
+std::string getLineWithQueryTag(container_type tagArray, container_type HRMLSourceCode);
 std::string getStringWithAttributValue(std::string attribute,
                                                  std::string lineWithQueryTag);
 std::string getAttributeValue(ParsedQuery parsedQuery,
@@ -48,20 +41,30 @@ int main()
     
     
     container_type HRMLSourceCode {
+                                    "<body>",
+                                    "<div id = \"myDiv\">",
+                                    "<h1 id = \"myHeading\">",
+                                    "</h1>",
+                                    "</div>",
+                                    "</body>",
+
                                     "<tag1 value = \"HelloWorld\">",
                                     "<tag2 name = \"Name1\">",
                                     "</tag2>",
-                                    "</tag1" };
+                                    "</tag1" 
+                                };
+
     container_type queries {
-                            "<tag1~value",
-                            "<tag2~name" };
+                            "div~id",
+                            "body.div~id",
+                            "h1~id",
+                            "div.h1~id",
+                            "body.div.h1~id",
 
+                            "tag1~value",
+                            "tag1.tag2~name" };
 
-    ParsedTags parsedTags{ getParsedTags("tag1.tag2.tag3~value") };
-    printParsedTags(parsedTags);
-
-
-    // printAttributeValues(HRMLSourceCode, queries);
+    printAttributeValues(HRMLSourceCode, queries);
     
     return 0;
 }
@@ -116,65 +119,46 @@ ParsedQuery getParsedQuery(query_type query)
 {
     ParsedQuery parsedQuery{};
 
-    parsedQuery.tag = query.substr(0, query.find("~"));
     parsedQuery.attribute = query.substr(query.find("~") + 1);
-
-    return parsedQuery;
-}
-
-ParsedTags getParsedTags(std::string tag)
-{
-    ParsedTags parsedTags;
 
     while (true)
     {
-        size_t pos{ tag.find(".") };
-        size_t pos2{ tag.find("~") };
+        size_t pos{ query.find(".") };
+        size_t pos2{ query.find("~") };
 
         if (pos != std::string::npos)
         {
-            // Extract the first tag and push it in the array
-            std::string parsedTag { tag.substr(0, tag.length() - tag.substr(pos).length()) };
-            parsedTags.tagArray.push_back(parsedTag);
-            // assign tag the value of remaining unparsed tag
-            tag = tag.substr(pos + 1);
+            // Extract the first query and push it in the array
+            std::string parsedTag { query.substr(0, query.length() - query.substr(pos).length()) };
+            parsedQuery.tagArray.push_back(parsedTag);
+            // assign query the value of remaining unparsed query
+            query = query.substr(pos + 1);
         }
         else if (pos2 != std::string::npos)
         {
-            // Extract the first tag and push it in the array
-            std::string parsedTag { tag.substr(0, tag.length() - tag.substr(pos2).length()) };
-            parsedTags.tagArray.push_back(parsedTag);
-            // assign tag the value of remaining unparsed tag
-            tag = tag.substr(pos2 + 1);
+            // Extract the first query and push it in the array
+            std::string parsedTag { query.substr(0, query.length() - query.substr(pos2).length()) };
+            parsedQuery.tagArray.push_back(parsedTag);
+            // assign query the value of remaining unparsed query
+            query = query.substr(pos2 + 1);
         }
         else
             break;
     }
-    
-    return parsedTags;
+
+    return parsedQuery;
 }
 
-void printParsedTags(ParsedTags parsedTags)
+std::string getLineWithQueryTag(container_type tagArray, container_type HRMLSourceCode)
 {
-    std::cout << '\n';
-    for (auto parsedTag : parsedTags.tagArray)
-    {
-        std::cout << parsedTag << '\n';
-    }
-    std::cout << '\n';
-}
+    std::string tag = tagArray.back();
 
-std::string getLineWithQueryTag(std::string tag, container_type HRMLSourceCode)
-{
-    // ParsedTag parsedTag{ getParsedTag(tag) };
-
+    int lineNumber{0};
     for (auto codeLine : HRMLSourceCode)
     {
-        int lineNumber{0};
-
         std::size_t found{ codeLine.find(tag) };
 
-        if (found!=std::string::npos)
+        if (found != std::string::npos)
         {
             return HRMLSourceCode[lineNumber];
         }
@@ -200,10 +184,10 @@ std::string getStringWithAttributValue(std::string attribute,
 std::string getAttributeValue(ParsedQuery parsedQuery,
                                                 container_type HRMLSourceCode)
 {
-    std::string tag{ parsedQuery.tag };
     std::string attribute{ parsedQuery.attribute };
+    container_type tagArray{ parsedQuery.tagArray };
 
-    std::string lineWithQueryTag { getLineWithQueryTag(tag, HRMLSourceCode) };
+    std::string lineWithQueryTag { getLineWithQueryTag(tagArray, HRMLSourceCode) };
 
     std::string stringWithAttributeValue{ 
                        getStringWithAttributValue(attribute, lineWithQueryTag) };
@@ -215,7 +199,9 @@ void printAttributeValues(container_type HRMLSourceCode, container_type queries)
 {
     for (auto query : queries)
     {
+
         ParsedQuery parsedQuery { getParsedQuery(query) };
         std::cout << getAttributeValue(parsedQuery, HRMLSourceCode) << '\n';
     }
 }
+
