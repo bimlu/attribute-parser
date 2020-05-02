aa89969451fb0ed0a72f2d8bb2faba24e200927b
robinsrz <35960806+robinsrz@users.noreply.github.com>
2020-05-02 17:46:45 +0530

    Update main.cpp
    final working version of the problem.
    problem on hackerRank.com

--- main.cpp
+++ main.cpp
@@ -3,205 +3,314 @@
 #include <array>
 #include <string>
 
-using container_type = std::vector<std::string>;
-using line_type = std::string;
-using query_type = std::string;
+class FindQuery;
 
-struct ParsedQuery
+class SourceCode
 {
-    container_type tagArray;
-    query_type attribute{};
+public:
+    using sourceCode_container_type = std::vector<std::string>;
+
+private:
+    sourceCode_container_type m_sourceCodeContainer{};
+
+public:
+    SourceCode(int numberOfLines = 0);
+    sourceCode_container_type fillSourceCodeContainer(int numberOfLines);
+    void print() const;
+    sourceCode_container_type getSourceCodeContainer() const;   
+
+    friend class FindQuery;
 };
 
-container_type getHRMLSourceCode(int numberOfLines);
-void printHRMLSourceCode(container_type HRMLSourceCode);
-container_type getQueries(int numberOfQuery);
-void printQueries(container_type queries);
-ParsedQuery getParsedQuery(query_type query);
-std::string getLineWithQueryTag(container_type tagArray, container_type HRMLSourceCode);
-std::string getStringWithAttributValue(std::string attribute,
-                                                 std::string lineWithQueryTag);
-std::string getAttributeValue(ParsedQuery parsedQuery,
-                                                container_type HRMLSourceCode);
-void printAttributeValues(container_type HRMLSourceCode, container_type queries);
+class RawQuery
+{
+public:
+    using rawQuery_container_type = std::vector<std::string>;
+
+private:
+    rawQuery_container_type m_rawQueryContainer{};
+
+public:
+    RawQuery(int numberOfLines = 0);
+    rawQuery_container_type fillRawQueryContainer(int numberOfLines);
+    void print() const;
+    rawQuery_container_type getRawQueryContainer() const;
+    
+};
+
+class ParsedQuery
+{
+public:
+    using rawQuery_container_type = std::vector<std::string>;
+    using parsedQuery_container_type = std::vector<std::vector<std::string>>;
+
+private:
+    // contains contaiers of tags
+    parsedQuery_container_type m_parsedQueryContainer{};
+
+public:
+    // Constructor initializes m_parsedQueryContainer
+    ParsedQuery(const RawQuery &rawQuery);
+
+    // prints all the elements in m_parsedQueryContainer
+    void print() const;
+
+    // returns private members
+    parsedQuery_container_type getParsedQueryContainer() const
+    {
+        return m_parsedQueryContainer;
+    }
+
+    // fills empty m_parsedQueryContainer with containers of tags
+    parsedQuery_container_type fillparsedQueryContainer(const RawQuery &rawQuery) const;
+
+    // make FindQuery class a friend
+    friend class FindQuery;
+};
+
+class FindQuery
+{
+public:
+    using sourceCode_container_type = std::vector<std::string>;
+    using parsedQuery_container_type = std::vector<std::vector<std::string>>;
+    using parsedCodeLines_container_type = std::vector<std::string>;
+    
+private:
+    // contains the source-code
+    sourceCode_container_type m_sourceCodeContainer{};
+    // contains parsed query
+    parsedQuery_container_type m_parsedQueryContainer{};
+
+public:
+    FindQuery(SourceCode &sourceCode, ParsedQuery &parsedQuery);
+    parsedCodeLines_container_type findParsedCodeLines(const std::vector<std::string> &tagArray) const;
+    std::string getAttrValue(const std::vector<std::string> &tagArray) const;
+    void printAttrValues() const;
+    
+};
 
 int main()
 {
-    // int N{};
-    // int Q{};
-    // std::cin >> N >> Q;
-    // std::cin.ignore(32767, '\n');
+    int N{};
+    int Q{};
+    std::cin >> N >> Q;
+    std::cin.ignore(32767, '\n');
 
-    // // Get the HRML Source code
-    // container_type HRMLSourceCode{ getHRMLSourceCode(N) };
-
-    // // Get the Queries
-    // container_type queries{ getQueries(Q) };
-
-    
-    
-    container_type HRMLSourceCode {
-                                    "<body>",
-                                    "<div id = \"myDiv\">",
-                                    "<h1 id = \"myHeading\">",
-                                    "</h1>",
-                                    "</div>",
-                                    "</body>",
-
-                                    "<tag1 value = \"HelloWorld\">",
-                                    "<tag2 name = \"Name1\">",
-                                    "</tag2>",
-                                    "</tag1" 
-                                };
-
-    container_type queries {
-                            "div~id",
-                            "body.div~id",
-                            "h1~id",
-                            "div.h1~id",
-                            "body.div.h1~id",
-
-                            "tag1~value",
-                            "tag1.tag2~name" };
-
-    printAttributeValues(HRMLSourceCode, queries);
+    // Get the HRML Source code
+    SourceCode sourceCode{ N };
+    // Get the Raw-Queries
+    RawQuery rawQuery{ Q };
+    // Parse Query
+    ParsedQuery parsedQuery{ rawQuery };
+    // Find parsed-query in parsed-code
+    FindQuery findQuery{ sourceCode, parsedQuery };
+    // print answer
+    findQuery.printAttrValues();
     
     return 0;
 }
 
-container_type getHRMLSourceCode(int numberOfLines)
+SourceCode::SourceCode(int numberOfLines)
 {
-    container_type HRMLSourceCode(numberOfLines);
+    m_sourceCodeContainer = fillSourceCodeContainer(numberOfLines);
+}
+
+SourceCode::sourceCode_container_type SourceCode::fillSourceCodeContainer(int numberOfLines)
+{
+    sourceCode_container_type sourceCodeContainer(numberOfLines);
     for (int count{0}; count < numberOfLines; ++count)
     {
-        std::string line;
-        std::getline(std::cin, line);
-        HRMLSourceCode[count] = line;
+        std::string sourceCodeLine;
+        std::getline(std::cin, sourceCodeLine);
+        sourceCodeContainer[count] = sourceCodeLine;
     }
-
-    return HRMLSourceCode;
+    return sourceCodeContainer;
 }
 
-void printHRMLSourceCode(container_type HRMLSourceCode)
+void SourceCode::print() const
 {
-    std::cout << '\n';
-    for (auto line : HRMLSourceCode)
-    {
-        std::cout << line << '\n';
-    }
-    std::cout << '\n';
-}
+    std::cout << "\nThis is Source code:\n";
 
-container_type getQueries(int numberOfQuery)
-{
-    container_type queries(numberOfQuery);
-    for (int count{0}; count < numberOfQuery; ++count)
+    for (const auto &sourceCodeLine : m_sourceCodeContainer)
     {
-        std::string query;
-        std::getline(std::cin, query);
-        queries[count] = query;
-    }
-
-    return queries;
-}
-
-void printQueries(container_type queries)
-{
-    std::cout << '\n';
-    for (auto query : queries)
-    {
-        std::cout << query << '\n';
+        std::cout << sourceCodeLine << '\n';
     }
     std::cout << '\n';
 }
 
-ParsedQuery getParsedQuery(query_type query)
+SourceCode::sourceCode_container_type SourceCode::getSourceCodeContainer() const
 {
-    ParsedQuery parsedQuery{};
+    return m_sourceCodeContainer;
+}
 
-    parsedQuery.attribute = query.substr(query.find("~") + 1);
+RawQuery::RawQuery(int numberOfLines)
+{
+    m_rawQueryContainer = fillRawQueryContainer(numberOfLines);
+}
 
-    while (true)
+RawQuery::rawQuery_container_type RawQuery::fillRawQueryContainer(int numberOfLines)
+{
+    rawQuery_container_type rawQueryContainer(numberOfLines);
+    for (int count{0}; count < numberOfLines; ++count)
     {
-        size_t pos{ query.find(".") };
-        size_t pos2{ query.find("~") };
+        std::string rawQueryLine;
+        std::getline(std::cin, rawQueryLine);
+        rawQueryContainer[count] = rawQueryLine;
+    }
+    return rawQueryContainer;
+}
 
+void RawQuery::print() const
+{
+    std::cout << "\nThis is Raw-query:\n";
+    for (const auto &rawQueryLine : m_rawQueryContainer)
+    {
+        std::cout << rawQueryLine << '\n';
+    }
+    std::cout << '\n';
+}
+
+RawQuery::rawQuery_container_type RawQuery::getRawQueryContainer() const
+{
+    return m_rawQueryContainer;
+}
+
+ParsedQuery::ParsedQuery(const RawQuery &rawQuery)
+{
+    // initialize member variable
+    m_parsedQueryContainer = fillparsedQueryContainer(rawQuery);
+}
+
+// print parsed query
+void ParsedQuery::print() const
+{
+    std::cout << "\nThis is Parsed-Query:\n";
+    for (const auto &parsedQueryLine : m_parsedQueryContainer)
+    {
+        for (const auto &tag : parsedQueryLine)
+        {
+            std::cout << tag << '*';
+        }
+        std::cout << '\n';
+    }
+    std::cout << '\n';
+}
+
+ParsedQuery::parsedQuery_container_type ParsedQuery::fillparsedQueryContainer(const RawQuery &rawQuery) const
+{
+    // parsedQueryContainer contains tagArray
+    // (each tagArray contains all the tags extracted from single rawQueryLine)
+    parsedQuery_container_type parsedQueryContainer{};
+
+    // rawQueryContainer is array of strings
+    // it contains all the raw queries
+    rawQuery_container_type rawQueryContainer{ rawQuery.getRawQueryContainer() };
+
+    for (auto &rawQueryLine : rawQueryContainer)
+    {
+        // tagArray contains all tags (including attribute name after ~ sign) in rawQueryLine
+        std::vector<std::string> tagArray{};
+
+        while (true)
+        {
+            size_t pos1{ rawQueryLine.find(".") };
+            size_t pos2{ rawQueryLine.find("~") };
+
+            if (pos1 != std::string::npos)
+            {
+                // Extract tagNames from rawQueryLine and store it in tag variable
+                std::string tag { rawQueryLine.substr(0, rawQueryLine.length() - 
+                                                        rawQueryLine.substr(pos1).length()) };
+                // push the tag in tagArray
+                tagArray.push_back(tag);
+                // assign rawQueryLine the value of remaining unparsed rawQueryLine
+                rawQueryLine = rawQueryLine.substr(pos1 + 1);
+            }
+            else if (pos2 != std::string::npos)
+            {
+                // Extract tagNames from rawQueryLine and store it in tag variable
+                std::string tag { rawQueryLine.substr(0, rawQueryLine.length() - 
+                                                        rawQueryLine.substr(pos2).length()) };
+                // push the tag in tagArray
+                tagArray.push_back(tag);
+                // assign rawQueryLine the value of remaining unparsed rawQueryLine
+                rawQueryLine = rawQueryLine.substr(pos2 + 1);
+            }
+            else
+            {   
+                // rawQueryLine contains the last tag atfter ~ sign
+                // push it in the tagArray too
+                tagArray.push_back(rawQueryLine);
+                break;
+            }
+        }
+
+        // push tagArray in the parsedQueryContainer
+        parsedQueryContainer.push_back(tagArray);
+    }
+    return parsedQueryContainer;
+}  
+
+FindQuery::FindQuery(SourceCode &sourceCode, ParsedQuery &parsedQuery)
+{
+    this->m_sourceCodeContainer = sourceCode.m_sourceCodeContainer;
+    this->m_parsedQueryContainer = parsedQuery.m_parsedQueryContainer;
+}
+
+FindQuery::parsedCodeLines_container_type FindQuery::findParsedCodeLines(
+                                                   const std::vector<std::string> &tagArray) const
+{
+    // this array contains sourceCodeLine with the tag (tagArray[second last element])
+    parsedCodeLines_container_type parsedCodeLines{};
+    // tag is second last element of the tagArary
+    std::string tag{ "<" + tagArray[tagArray.size() - 2] };
+
+    for (const auto &sourceCodeLine : m_sourceCodeContainer)
+    {
+        std::size_t pos{ sourceCodeLine.find(tag) };
         if (pos != std::string::npos)
         {
-            // Extract the first query and push it in the array
-            std::string parsedTag { query.substr(0, query.length() - query.substr(pos).length()) };
-            parsedQuery.tagArray.push_back(parsedTag);
-            // assign query the value of remaining unparsed query
-            query = query.substr(pos + 1);
+            parsedCodeLines.push_back(sourceCodeLine);
         }
-        else if (pos2 != std::string::npos)
+    }
+    
+    return parsedCodeLines;
+}
+
+std::string FindQuery::getAttrValue(const std::vector<std::string> &tagArray) const
+{
+    std::string attrName{ tagArray.back() };
+
+    std::vector<std::string> parsedCodeLines = findParsedCodeLines(tagArray);
+
+    for (auto &line : parsedCodeLines)
+    {
+        std::size_t pos{ line.find(attrName) };         
+        if (pos != std::string::npos)
         {
-            // Extract the first query and push it in the array
-            std::string parsedTag { query.substr(0, query.length() - query.substr(pos2).length()) };
-            parsedQuery.tagArray.push_back(parsedTag);
-            // assign query the value of remaining unparsed query
-            query = query.substr(pos2 + 1);
+            line = line.substr(pos + attrName.size() + 4) ;
+            std::size_t pos2{ line.find("\"") };
+            if (pos2 != std::string::npos)
+            {
+                std::string attrValue{ line.substr(0, line.length() - line.substr(pos2).length()) };
+                return attrValue;
+            }
         }
+    }
+    return ""; 
+}
+
+void FindQuery::printAttrValues() const
+{   
+    std::string attrValue{};
+
+    for (const auto &tagArray : m_parsedQueryContainer)
+    {
+        attrValue = getAttrValue(tagArray);
+
+        if (attrValue == "")
+            std::cout << "Not Found!\n";
         else
-            break;
-    }
-
-    return parsedQuery;
+            std::cout << attrValue << '\n';
+    }     
 }
-
-std::string getLineWithQueryTag(container_type tagArray, container_type HRMLSourceCode)
-{
-    std::string tag = tagArray.back();
-
-    int lineNumber{0};
-    for (auto codeLine : HRMLSourceCode)
-    {
-        std::size_t found{ codeLine.find(tag) };
-
-        if (found != std::string::npos)
-        {
-            return HRMLSourceCode[lineNumber];
-        }
-
-        ++lineNumber;
-    }
-
-    return " ";
-}
-
-std::string getStringWithAttributValue(std::string attribute,
-                                                  std::string lineWithQueryTag)
-{
-    std::size_t pos{ lineWithQueryTag.find(attribute) + attribute.size() + 4 };
-
-    std::string str{ lineWithQueryTag.substr(pos) };
-
-    str.erase(str.size() - 2);
-
-    return str;
-}
-
-std::string getAttributeValue(ParsedQuery parsedQuery,
-                                                container_type HRMLSourceCode)
-{
-    std::string attribute{ parsedQuery.attribute };
-    container_type tagArray{ parsedQuery.tagArray };
-
-    std::string lineWithQueryTag { getLineWithQueryTag(tagArray, HRMLSourceCode) };
-
-    std::string stringWithAttributeValue{ 
-                       getStringWithAttributValue(attribute, lineWithQueryTag) };
-
-    return stringWithAttributeValue;
-}
-
-void printAttributeValues(container_type HRMLSourceCode, container_type queries)
-{
-    for (auto query : queries)
-    {
-
-        ParsedQuery parsedQuery { getParsedQuery(query) };
-        std::cout << getAttributeValue(parsedQuery, HRMLSourceCode) << '\n';
-    }
-}
-
